1fe460ed47497223976157d902d3c8acc0bf0290
Angel <angel.hdz.ojd@gmail.com>
2025-01-13 16:05:18 +0000

    Ended login page

--- app/src/main/java/com/example/mentoriasapp/activity/LoginActivity.kt
+++ app/src/main/java/com/example/mentoriasapp/activity/LoginActivity.kt
@@ -1,13 +1,18 @@
 package com.example.mentoriasapp.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import com.example.mentoriasapp.MainActivity
 import com.example.mentoriasapp.R
 import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import kotlin.math.log
 
 class LoginActivity : ComponentActivity() {
@@ -19,20 +24,45 @@ class LoginActivity : ComponentActivity() {
         val editTextEmail: EditText = findViewById(R.id.editTextTextEmailAddress)
         val editTextPassword: EditText = findViewById(R.id.editTextTextPassword)
         val loginButton: Button = findViewById(R.id.login_button)
+        lateinit var auth: FirebaseAuth
+        auth = Firebase.auth
 
         loginButton.setOnClickListener {
             val email = editTextEmail.text.toString()
             val password = editTextPassword.text.toString()
 
+            //Comprobamos si esta vacio
             if (email.isEmpty()) {
                 Toast.makeText(this@LoginActivity, "Por favor introduzca usuario", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-
             if (password.isEmpty()) {
                 Toast.makeText(this@LoginActivity, "Por favor introduzca la contraseña", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            baseContext,
+                            "Inicio de sesión satisfactorio!",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                        val user = auth.currentUser
+
+                        //Se redirige al MainActivity---------
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        Toast.makeText(
+                            baseContext,
+                            "Inicio de sesión fallido!",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
         }
     }
 }
--- app/src/main/java/com/example/mentoriasapp/activity/RegisterActivity.kt
+++ app/src/main/java/com/example/mentoriasapp/activity/RegisterActivity.kt
@@ -9,7 +9,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.mentoriasapp.MainActivity
 import com.example.mentoriasapp.R
+import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
